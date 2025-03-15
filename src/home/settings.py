0549571b175a78e7118b5235c47ec8de2daf4d1c
oluoko <brianotieno586@gmail.com>
2025-03-15 09:36:53 +0300

    Saas Django: Preparing for Railway Production

--- src/home/settings.py
+++ src/home/settings.py
@@ -25,7 +25,15 @@ SECRET_KEY = 'django-insecure-sist_agtt-m-@hj^^dttt&c2)-i+ebuiw^qzn49739zs3)nsv1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app"
+]
+
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost",
+    ]
 
 
 # Application definition
