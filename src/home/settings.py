f1ea0b551f3d5cc7e99147c0437a88cb35b765cb
oluoko <brianotieno586@gmail.com>
2025-03-16 20:19:43 +0300

    Saas Django: Added Django DEBUG environ

--- src/home/settings.py
+++ src/home/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sist_agtt-m-@hj^^dttt&c2)-i+ebuiw^qzn49739zs3)nsv1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DJANGO_DEBUG") ).lower() == "true"
+print("DEBUG:", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app"
