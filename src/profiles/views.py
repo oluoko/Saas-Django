2c9a51f012782794ab9ce2f9ea61d0045b58b03f
oluoko <brianotieno586@gmail.com>
2025-06-14 18:50:30 +0300

    Saas Django: User Permissions and has_perm

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -8,9 +8,23 @@ User = get_user_model()
 @login_required
 def profile_view(request,  username=None, *args, **kwargs):
     user = request.user
+
+    print('user.has_perm("auth.view_user")', user.has_perm("auth.view_user"))
+    print('user.has_perm("visits.view_pagevisit")', user.has_perm("visits.view_pagevisit"))
+
+    # <app_label>.view_<model_name>
+    # <app_label>.add_<model_name>
+    # <app_label>.change_<model_name>
+    # <app_label>.delete_<model_name>
+
     # profile_user_obj = User.objects.get(username=username)
     profile_user_obj = get_object_or_404(User, username=username)
 
     is_me = profile_user_obj == user
+
+    if is_me: 
+        if user.has_perm("visits.view_pagevisit"):
+            print("You have permission to view your own profile and page visits.")
+            
 
     return HttpResponse(f"Hello World! This is the profile view for {username} - {profile_user_obj.id} - {user.id} - {is_me}")
