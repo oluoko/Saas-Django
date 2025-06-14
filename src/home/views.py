486e36a70752f151e6ff656f8a083a3172e56e15
oluoko <brianotieno586@gmail.com>
2025-06-14 16:28:01 +0300

    Saas Django: User-required Pages via login_required Decorator

--- src/home/views.py
+++ src/home/views.py
@@ -1,5 +1,6 @@
 import pathlib
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 from visits.models import PageVisit
@@ -53,7 +54,7 @@ VALID_CODE = "abc123"
 
 def pw_protected_view(request, *args, **kwargs):
     is_allowed = request.session.get('protected_page_allowed') or 0
-    print(request.session.get('protected_page_allowed'), type(request.session.get('protected_page_allowed')))
+    # print(request.session.get('protected_page_allowed'), type(request.session.get('protected_page_allowed')))
     if request.method == "POST":
         user_pw_sent = request.POST.get("code") or None
         if user_pw_sent == VALID_CODE:
@@ -62,3 +63,9 @@ def pw_protected_view(request, *args, **kwargs):
     if is_allowed:
         return render(request, "protected/view.html", {})
     return render(request, "protected/entry.html", {})
+
+
+
+@login_required
+def user_only_view(request, *args, **kwargs):
+    return render(request, "protected/user-only.html", {})
